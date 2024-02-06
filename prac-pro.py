ec0d26791cb5a736ee4f2444a109c664727340ba
Govind <govindbasnet02@gmail.com>
2024-02-06 13:53:22 +0545

    Feat: infinite loop

--- prac-pro.py
+++ prac-pro.py
@@ -1,2 +1,6 @@
-name = input("Enter Number: ")
-print(name)
+User = input("Enter your name: ")
+while(User == "Govind"):
+     print("Your name is correct ")
+     User = User 
+else:
+     print("Your Name is incorrect")
