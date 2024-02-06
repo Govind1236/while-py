1a216f62dae7cc449d709c67921a1bb409dc0d58
Govind <govindbasnet02@gmail.com>
2024-02-06 19:48:53 +0545

    feat: add while in password checker

---
+++ password-check.py
@@ -0,0 +1,11 @@
+pass_word = "Govind@123"
+
+while True:
+    password = input("Enter a password: ")
+    if password == pass_word:
+        print("Correct")
+        break
+    else:
+        print("Enter correct Password")
+        
+    
