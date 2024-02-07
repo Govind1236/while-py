416221bb9a82b41b166d918491c7277640806611
Govind <govindbasnet02@gmail.com>
2024-02-07 18:35:01 +0545

    Fix: add array of num

--- average.py
+++ average.py
@@ -2,6 +2,5 @@ def cal_avg(numbers):
     
     total = sum(numbers) / len(numbers)
     return total
-avg = [12 ,12, 10]
-result_avg = cal_avg(avg)
-print("The average is:",result_avg)
+numbers = [10 , 3 ,4]
+print("The average is: ", cal_avg(numbers))
