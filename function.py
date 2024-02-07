c335bc7e17d5759f6a7107ded8daff805016b7c2
Govind <govindbasnet02@gmail.com>
2024-02-07 17:14:30 +0545

    Fix: add greater and smaller using functions

--- function.py
+++ function.py
@@ -1,6 +1,12 @@
 # functions------
 def functions(a,b):
- return int(a + b)
+  sum1 = a +b
+  print(sum1)
 a = 10
 b =12
+if (a > b):
+  print("a is greater", a)
+else:
+  print("b is greater", b)
 functions(a , b)
+
